686670ec65c9dc09bef8d2d6f8e97ff431bd919b
IagoOlivares <iago2002oc@gmail.com>
2024-11-21 14:32:21 +0100

    Ejercicio 18 completado

--- src/main/java/com/campusdual/classroom/Exercise18.java
+++ src/main/java/com/campusdual/classroom/Exercise18.java
@@ -3,6 +3,27 @@ package com.campusdual.classroom;
 public class Exercise18 {
 
 	public static void main(String[] args) {
+		int[] array = new int[5];
+		array=  createAndInitializeArray(array.length);
+		showInlineArray(array);
+	}
 
+	public static int[] createAndInitializeArray(int numero){
+
+		int[] array = new int[numero];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = i+1;
+		}
+		return array;
+	}
+
+	public static void showInlineArray(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			if (i < array.length - 1) {
+				System.out.print(array[i] + " ");
+			} else {
+				System.out.print(array[i]);
+			}
+		}
 	}
 }
